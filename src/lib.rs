54e88f912b215ab2afd78efc6f578917f3100570
Grzegorz <grzegorz.bartoszek@thaumatec.com>
2019-05-21 12:13:14 +0200

    ascii art tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,68 +6,95 @@ const HEIGHT: usize = 10;
 enum Square {
     Snake,
     Fruit,
-    Empty
+    Empty,
 }
 
 trait Board: Default {
     fn width(&self) -> usize;
     fn height(&self) -> usize;
-    fn at(&mut self, x: usize, y: usize) -> &mut Square;
+    fn at_mut(&mut self, x: usize, y: usize) -> &mut Square;
+    fn at(&self, x: usize, y: usize) -> Square;
 }
 
 struct FixedSizeBoard
 {
-    data : [Square; WIDTH * HEIGHT]
+    data: [Square; WIDTH * HEIGHT]
 }
 
 impl Default for FixedSizeBoard {
     fn default() -> Self {
-        Self{data: [Square::Empty; WIDTH * HEIGHT]}
+        Self { data: [Square::Empty; WIDTH * HEIGHT] }
     }
 }
 
 impl Board for FixedSizeBoard {
     fn width(&self) -> usize { WIDTH }
     fn height(&self) -> usize { HEIGHT }
-    fn at(&mut self, x: usize, y: usize) -> &mut Square {
-        dbg!(x);
-        dbg!(y);
-        &mut self.data[dbg!(y*self.width() + x)]
+    fn at_mut(&mut self, x: usize, y: usize) -> &mut Square {
+        x;
+        y;
+        &mut self.data[y * self.width() + x]
+    }
+    fn at(&self, x: usize, y: usize) -> Square {
+        x;
+        y;
+        self.data[y * self.width() + x]
     }
 }
 
 struct Game<B> where B: Board {
     width: usize,
     height: usize,
-    _pd: std::marker::PhantomData<B>
+    _pd: std::marker::PhantomData<B>,
 }
 
 impl<B> Game<B> where B: Board {
     pub fn new(width: usize, height: usize) -> Game<B> {
-        Game{width, height, _pd: std::marker::PhantomData::<B>{}}
+        Game { width, height, _pd: std::marker::PhantomData::<B> {} }
     }
 
     pub fn board(&self) -> impl Board {
         let mut board = B::default();
 
-        let center_x = self.width / 2;
-        let center_y = self.height / 2;
+        let center_x = dbg!(self.width / 2);
+        let center_y = dbg!(self.height / 2);
 
-        *board.at(center_x, center_y) = Square::Snake;
-        *board.at(center_x - 1, center_y) = Square::Snake;
+        *board.at_mut(center_x, center_y) = Square::Snake;
+        *board.at_mut(center_x - 1, center_y) = Square::Snake;
 
         board
     }
 
-    pub fn advance(&self) {
-
-    }
+    pub fn advance(&self) {}
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::error::Error;
+
+    fn check_board(board: &impl Board, expected: &Vec<String>) -> Vec<String> {
+        assert_eq!(board.height(), expected.len(), "Invalid height");
+
+        expected.iter().enumerate().map(|(y, row)| -> Vec<String> {
+            assert_eq!(board.width(), row.chars().count(), "Invalid width");
+
+            row.chars().enumerate().map(|(x, square)| {
+                let expected = match square {
+                    '◯' => Square::Snake,
+                    'F' => Square::Fruit,
+                    _ => Square::Empty,
+                };
+
+                if board.at(x, y) != expected {
+                    Err(format!("X:{} Y:{} should be {:?} but is {:?}", x, y, expected, board.at(x, y)))
+                } else {
+                    Ok(())
+                }
+            }).filter_map(Result::err).collect()
+        }).flatten().collect()
+    }
 
     #[test]
     fn game_is_initialized() {
@@ -78,13 +105,22 @@ mod tests {
     fn at_the_beginning_snake_is_in_the_middle() {
         let game = Game::<FixedSizeBoard>::new(WIDTH, HEIGHT);
 
-        let center_x = WIDTH / 2;
-        let center_y = HEIGHT / 2;
-
         let mut board = game.board();
 
-        assert_eq!(Square::Snake, *board.at(center_x, center_y));
-        assert_eq!(Square::Snake, *board.at(center_x+1, center_y));
+        let expected = vec![
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+            String::from("    ◯◯    "),
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+        ];
+
+        assert_eq!(Vec::<String>::new(), check_board(&board, &expected));
     }
 
     #[test]
@@ -98,8 +134,19 @@ mod tests {
 
         let mut board = game.board();
 
-        assert_eq!(Square::Empty, *board.at(center_x+0, center_y));
-        assert_eq!(Square::Snake, *board.at(center_x+1, center_y));
-        assert_eq!(Square::Snake, *board.at(center_x+2, center_y));
+        let expected = vec![
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+            String::from("     ◯◯   "),
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+            String::from("          "),
+        ];
+
+        assert_eq!(Vec::<String>::new(), check_board(&board, &expected));
     }
 }
